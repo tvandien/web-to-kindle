87b0cee96797d5e9897a1cd60748df45d92552a1
tijmend-bls <tijmend@bottomline.eu>
2020-07-28 21:19:19 +0200

    Add controllers

---
+++ WebToKindle/Controllers/BookTemplatesController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebToKindle.Database;
+using WebToKindle.Database.Tables;
+
+namespace WebToKindle.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookTemplatesController : ControllerBase
+    {
+        private readonly WebToKindleDB _context;
+
+        public BookTemplatesController(WebToKindleDB context)
+        {
+            _context = context;
+        }
+
+        // GET: api/BookTemplates
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<BookTemplate>>> GetBookTemplates()
+        {
+            return await _context.BookTemplates.ToListAsync();
+        }
+
+        // GET: api/BookTemplates/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<BookTemplate>> GetBookTemplate(int id)
+        {
+            var bookTemplate = await _context.BookTemplates.FindAsync(id);
+
+            if (bookTemplate == null)
+            {
+                return NotFound();
+            }
+
+            return bookTemplate;
+        }
+
+        // PUT: api/BookTemplates/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBookTemplate(int id, BookTemplate bookTemplate)
+        {
+            if (id != bookTemplate.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(bookTemplate).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BookTemplateExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/BookTemplates
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<BookTemplate>> PostBookTemplate(BookTemplate bookTemplate)
+        {
+            _context.BookTemplates.Add(bookTemplate);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetBookTemplate", new { id = bookTemplate.Id }, bookTemplate);
+        }
+
+        // DELETE: api/BookTemplates/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<BookTemplate>> DeleteBookTemplate(int id)
+        {
+            var bookTemplate = await _context.BookTemplates.FindAsync(id);
+            if (bookTemplate == null)
+            {
+                return NotFound();
+            }
+
+            _context.BookTemplates.Remove(bookTemplate);
+            await _context.SaveChangesAsync();
+
+            return bookTemplate;
+        }
+
+        private bool BookTemplateExists(int id)
+        {
+            return _context.BookTemplates.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ WebToKindle/Controllers/RegexesController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebToKindle.Database;
+using WebToKindle.Database.Tables;
+
+namespace WebToKindle.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegexesController : ControllerBase
+    {
+        private readonly WebToKindleDB _context;
+
+        public RegexesController(WebToKindleDB context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Regexes
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Regex>>> GetRegexes()
+        {
+            return await _context.Regexes.ToListAsync();
+        }
+
+        // GET: api/Regexes/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Regex>> GetRegex(int id)
+        {
+            var regex = await _context.Regexes.FindAsync(id);
+
+            if (regex == null)
+            {
+                return NotFound();
+            }
+
+            return regex;
+        }
+
+        // PUT: api/Regexes/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutRegex(int id, Regex regex)
+        {
+            if (id != regex.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(regex).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!RegexExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Regexes
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Regex>> PostRegex(Regex regex)
+        {
+            _context.Regexes.Add(regex);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetRegex", new { id = regex.Id }, regex);
+        }
+
+        // DELETE: api/Regexes/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Regex>> DeleteRegex(int id)
+        {
+            var regex = await _context.Regexes.FindAsync(id);
+            if (regex == null)
+            {
+                return NotFound();
+            }
+
+            _context.Regexes.Remove(regex);
+            await _context.SaveChangesAsync();
+
+            return regex;
+        }
+
+        private bool RegexExists(int id)
+        {
+            return _context.Regexes.Any(e => e.Id == id);
+        }
+    }
+}
