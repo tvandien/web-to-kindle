239a885bc3017a3283e1b72e4b49f99d87c87551
tijmend-bls <tijmend@bottomline.eu>
2020-07-28 21:18:14 +0200

    Add migrations and example mssql sa password file

---
+++ WebToKindle/Migrations/20200727204604_Migration-Add-Chapter-ChapterNumber.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebToKindle.Migrations
+{
+    public partial class MigrationAddChapterChapterNumber : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ChapterNumber",
+                table: "Chapters",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ChapterNumber",
+                table: "Chapters");
+        }
+    }
+}
